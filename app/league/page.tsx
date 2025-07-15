cb4385caba0fb89a246f3caacc0eb3c4f2e165c5
Crazed8s <scottiemlewis1991@gmail.com>
2025-07-15 08:17:44 -0400

    Clear existing code for refactor

--- app/league/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function LeaguePage(){
-    return (
-        <main className="min-h-screen p-10 text-white bg-black">
-            <h1 className="text-4xl font-bold mb-4">Dadgic Magic Beer League</h1>
-            <p>This is the start of the DMBL page. Match data and leaderboard coming soon!</p>
-        </main>
-    );
-}
