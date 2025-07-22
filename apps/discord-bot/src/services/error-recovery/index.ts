5cef8cd7861e9be165bb278d93ce7930dd6c079c
Crazed8s <scottiemlewis1991@gmail.com>
2025-07-22 12:43:05 -0400

    Anotha One x3

--- apps/discord-bot/src/services/error-recovery/index.ts
+++ apps/discord-bot/src/services/error-recovery/index.ts
@@ -1,4 +1,4 @@
-export { ErrorRecoveryService } from './ErrorRecoveryService'
-export { GeminiRetryService } from './GeminiRetryService'
-export { ConversationRecoveryService } from './ConversationRecoveryService'
-export type { ErrorContext } from './ErrorRecoveryService'
+export { ErrorRecoveryService } from './ErrorRecoveryService.js'
+export { GeminiRetryService } from './GeminiRetryService.js'
+export { ConversationRecoveryService } from './ConversationRecoveryService.js'
+export type { ErrorContext } from './ErrorRecoveryService.js'
